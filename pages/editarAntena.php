c7a27cde49378252a8f1e7bc58b0410b2bc909fb
jrge <jorgemoymoya@gmail.com>
2026-01-23 16:21:23 -0300

    Editar antena: Se crea tabla para mostrar datos de antena. Se mejora función de cambio de estado.

--- pages/editarAntena.php
+++ pages/editarAntena.php
@@ -3,7 +3,8 @@ $titleName = "Editar Antena";
 require_once("header.php"); 
 require_once ("./../app/models/antenas.php");
 $id = $_GET['id'];
-
+$codigo =$_GET['codigo'];
+$estadoActual = $_GET['estado'];
 $antena = new Antenas();
 
 #Verifica si se envió el form. Envia alerta y redirige
@@ -19,21 +20,48 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
 ?>
 
-<h1>
-    <?php
-    echo $id;
-    ?>
-</h1>
+<div class="container">
+    
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Código antena</th>
+                <th>Estado actual</th>
+                <th>Accion</th>
+            </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>
+                <?php echo $codigo;?>
+            </td>
+            <form method="POST">
+            <td class="col-2">
+                
+                    <select name="cambio-estado" class="form-select" aria-label="Selecciones nuevo estado">
+                        <option selected>
+                            <?php echo $estadoActual;?>
+                        </option>
+                        <option value="activo">Activo</option>
+                        <option value="inactivo">Inactivo</option>
+                    </select>
 
-<form method="POST">
-    <select name="cambio-estado" class="form-select" aria-label="Selecciones nuevo estado">
-        <option selected>Cambiar Estado</option>
-        <option value="activo">Activo</option>
-        <option value="inactivo">Inactivo</option>
-    </select>
+                   
+                
+            </td>
+            <td>
+                 <button type="submit" class="btn btn-primary">
+                    Cambiar estado
+                    </button>
+            </td>
+            </form>
+        </tr>
+    </tbody>
+    </table>
+</div>
 
-    <button type="submit" class="btn btn-primary">
-    Cambiar estado
-</button>
-</form>
+    
+
+
+
 
